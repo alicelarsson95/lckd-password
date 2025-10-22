2e2a34c5c854f0657ee31224834752b29b5651fe
alicelarsson95 <alicelarsson95@hotmail.com>
2025-10-22 15:25:20 +0200

    CreatePassword

--- frontend/src/components/global/LogoItem.jsx
+++ frontend/src/components/global/LogoItem.jsx
@@ -2,9 +2,9 @@ import logo from "../../assets/lck-logo.png";
 
 function LogoItem({ title }) {
   return (
-    <div className="flex flex-col items-center gap-2 mb-6">
+    <div className="flex items-center gap-5 absolute top-6 left-6">
       <img src={logo} alt="LCKD logo" className="w-8 h-8" />
-      <h1 className="text-white font-semibold tracking-wide text-lg">{title}</h1>
+      <h1 className="text-white font-semibold tracking-wide text-2xl">{title}</h1>
     </div>
   );
 }
