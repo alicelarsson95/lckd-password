b8169a8f5bd2b4168ad8ca93ba889dbc55da4cc5
veleii <vendelanorinder@hotmail.se>
2025-10-22 13:27:03 +0200

    startatpå login och signup

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+export default function Login() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-[#0C056D]">
+      <div className="text-center text-[#FFB703]">
+        <h1 className="text-white text-4xl font-bold tracking-[0.3em] mb-2">
+          LCKD
+        </h1>
+        <p className="font-semibold tracking-wider">
+          KEEPING YOUR PASSWORDS SAFE
+        </p>
+      </div>
+    </div>
+  );
+}
